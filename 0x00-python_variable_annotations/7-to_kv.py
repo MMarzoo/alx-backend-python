1bf28f8427462fda7ce72751e452cc81159ed8f2
MMarzoo <mmarzouk12378@gmail.com>
2024-08-01 19:24:38 +0300

    task #7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Write a type-annotated function to_kv
+'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    ''' Returns a tuple with a string and a float'''
+    return (k, v**2)
